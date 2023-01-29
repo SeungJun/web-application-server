d788677e834001722f5d92e516cb04c9e50a85bb
Seungjun <ghdgn22@naver.com>
2023-01-29 21:04:59 +0900

    3-post_join

--- src/main/java/util/GetHttpHeader.java
+++ src/main/java/util/GetHttpHeader.java
@@ -1,5 +1,9 @@
 package util;
 
+import model.User;
+
+import java.util.Map;
+
 public class GetHttpHeader {
 
     // 클래스 이름을 어떻게 지어야 할지
@@ -11,4 +15,16 @@ public class GetHttpHeader {
             //첫번째 위치 인덱스 index.html가져옴
             return tokens[1];
     }
+
+    public static void GetJoin(String url){
+        // ? 로 requestPath와 params를 나눈다.
+        int index = url.indexOf("?");
+        String requestPath = url.substring(0,index);
+        String params = url.substring(index+1);
+        // Utills의 parseQueryString을 이용해 구분
+        Map<String,String> info = HttpRequestUtils.parseQueryString(params);
+
+        User user = new User(info.get("userId"),info.get("password"),info.get("name"),info.get("email"));
+    }
+
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.GetHttpHeader;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -36,37 +37,50 @@ public class RequestHandler extends Thread {
             String line = br.readLine();
 
             // line이 null값이면 예외처리
-            if(line == null) return;
+            if (line == null) return;
 
+            String[] tokens = line.split(" ");
+            // body의 데이터의 길이를 저장할 변수
+            int contentLength = 0;
+            while (!"".equals(line)) {
+                log.debug("header : {}", line);
+                // 라인을 한 줄씩 읽어온다.
+                line = br.readLine();
+                if (line.contains("Content-Length")) {
+                    // 헤더중 content-length가 포함 되어 있으면 저장
+                    String[] temp = line.split(":");
+                    contentLength = Integer.parseInt(temp[1].trim());
+                }
+            }
 
             log.debug("request : {}", line);
-            
 
 
+            // line을 계속 사용해서 더이상 토큰으로 구분 할 수없다.
             // 읽어온 HTTP 요청 정보에서 첫번째 줄에서 요청 URL을 가져온다.
-            String url = GetHttpHeader.GetHttpUrl(line);
-            log.debug("request : {}", url);
+//            String url = GetHttpHeader.GetHttpUrl(line);
+//            log.debug("request : {}", url);
+
+            String url = tokens[1];
 
             // GET /user/create?userId=t&password=t&name=t&email=t%40g HTTP/1.1
             // 정보가 위의 주석처럼 넘어오면 회원 가입 요청이다
-            if(url.startsWith("/user/create")){
-                // ? 로 requestPath와 params를 나눈다.
-                int index = url.indexOf("?");
-                String requestPath = url.substring(0,index);
-                String params = url.substring(index+1);
+            if (url.startsWith("/user/create")) {
+                String body = IOUtils.readData(br,contentLength);
                 // Utills의 parseQueryString을 이용해 구분
-                Map<String,String> info = HttpRequestUtils.parseQueryString(params);
+                Map<String, String> info = HttpRequestUtils.parseQueryString(body);
 
-                User user = new User(info.get("userId"),info.get("password"),info.get("name"),info.get("email"));
-                log.debug("request : {}",user);
+                User user = new User(info.get("userId"), info.get("password"), info.get("name"), info.get("email"));
+                log.debug("request : {}", user);
 
-            }
-            
-            // 요청 URL에 해당하는 파일을 wdbapp 디렉토리에서 읽어 전달한다.
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+            } else {
+                // 요청 URL에 해당하는 파일을 wdbapp 디렉토리에서 읽어 전달한다.
+                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 //            log.debug("request : {}", body);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
