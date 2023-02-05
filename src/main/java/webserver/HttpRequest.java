eaaf2ab56a4662769c7d5024377953eea7445cce
Seungjun <ghdgn22@naver.com>
2023-02-05 17:10:16 +0900

    1차 리팩토링 - 요청 데이터 처리하는 로직 분리

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,98 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
+    private String method;
+    private String path;
+
+    // 헤더 정보를 담는것
+    private Map<String, String> headers = new HashMap<String, String>();
+    // 내을 담는것
+    private Map<String, String> params = new HashMap<String, String>();
+
+    public HttpRequest(InputStream in) {
+        try {
+            // 요청받은 값을 인코딩 후 입력
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            // 한줄 읽기
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+            processRequestLine(line);
+
+            line = br.readLine();
+            // 한줄씩 읽어 헤더에 넣어준다
+            while (!line.equals(" ")){
+                log.debug("header : {}",line);
+                // : 기준으로 파라미터와 내용이 들어가 있다
+                String[] tokens = line.split(":");
+                // 헤더에 분리한 값을 넣어준다
+                headers.put(tokens[0].trim(),tokens[1].trim());
+                line = br.readLine();
+            }
+
+            if ("POST".equals(method)) {
+                String body = IOUtils.readData(br,
+                        Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        }
+        // 더 상위버전으로 에러 처리
+        catch (IOException io) {
+            log.error(io.getMessage());
+        }
+
+    }
+
+    private void processRequestLine(String requestLine) {
+        log.debug("requset line : {}", requestLine);
+        String[] tokens = requestLine.split(" ");
+        // 메소드 : POST, GET 방식인지 확인
+        method = tokens[0];
+
+
+        // 이부분이 왜 POST 인지
+        if ("POST".equals(method)) {
+            path = tokens[1];
+            return;
+        }
+
+        // ?를 기준으로 메소드와 파라미터가 나누어진다.
+        int index = tokens[1].indexOf("?");
+        // 왜 -1인지?
+        if (index == -1) {
+            path = tokens[1];
+
+        } else {
+            path = tokens[1].substring(0, index);
+            params = HttpRequestUtils.parseQueryString(tokens[1].substring(index + 1));
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getHeader(String name) {
+        return headers.get(name);
+    }
+
+    public String getParam(String name) {
+        return params.get(name);
+    }
+}
